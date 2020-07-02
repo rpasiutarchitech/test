4457af549e2b66b99064cf5304b29f216bf4161b
rpasiutarchitech <rpasiut@architech.ca>
2020-07-02 12:54:14 +0200

    simple schematics

--- tools/schematics/create-sample-complex-lib/index.ts
+++ tools/schematics/create-sample-complex-lib/index.ts
@@ -1,9 +1,30 @@
-import { chain, externalSchematic, Rule } from '@angular-devkit/schematics';
+import { apply, chain, externalSchematic, mergeWith, move, Rule, SchematicContext, Tree, url } from '@angular-devkit/schematics';
+import { getProjectConfig } from '@nrwl/workspace';
+
+function generateLibrary(schema: any): Rule {
+  return externalSchematic('@nrwl/workspace', 'lib', {
+    name: schema.name
+  });
+}
+
+function generateFiles(schema: any): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    context.logger.info('adding NOTES.md to lib');
+    context.logger.info(getProjectConfig(tree, `${schema.name}`).root);
+
+    const templateSource = apply(url('./files'), [
+      move(getProjectConfig(tree, `${schema.name}`).root)
+    ]);
+
+    return chain([mergeWith(templateSource)])(tree, context);
+  };
+}
 
 export default function(schema: any): Rule {
-  return chain([
-    externalSchematic('@nrwl/workspace', 'lib', {
-      name: schema.name
-    })
-  ]);
+  return (tree: Tree, context: SchematicContext) => {
+    return chain([generateLibrary(schema), generateFiles(schema)])(
+      tree,
+      context
+    );
+  };
 }
